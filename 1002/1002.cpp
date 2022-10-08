58a0a1749b2d323c512c68fe0c2add1402ee0503
Gypsophila1028 <1010611428@qq.com>
2022-10-08 22:36:55 +0800

    2022-10-8

--- 1002/1002.cpp
+++ 1002/1002.cpp
@@ -1,71 +1,73 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-int p_number (int x)
+#include <stdio.h>
+int main()
 {
-    switch(x)
+    int sum=0,i=0,n=0,test,b=0;
+    char a[101]={};
+    while(1)
     {
-        case 0: cout<<"ling"; break;
-        case 1: cout<<"yi"; break;
-        case 2: cout<<"er"; break;
-        case 3: cout<<"san"; break;
-        case 4: cout<<"si"; break;
-        case 5: cout<<"wu"; break;
-        case 6: cout<<"liu"; break;
-        case 7: cout<<"qi"; break;
-        case 8: cout<<"ba"; break;
-        case 9: cout<<"jiu"; break;
-        default: break;
+        scanf("%c",&a[i]);
+        if(a[i]>='0'&&a[i]<='9')
+            i++;
+        else break;
     }
-    return 0;
-}
+    a[i]='\0';
+    i=0;
 
-int count_1 (int x)//计算位数
-{
-    int s=0;
-    do {
-        s++;
-        x/=10;
-    }while(x);
-    return s;
-}
-
-int count_2(int x,int s)//计算累计值
-{
-    int a,all_1=0;
-    for(int i=0;i<x;i++)
+    while(a[i]!='\0')
     {
-        a = s%10;
-        s/=10;
-        all_1=all_1+a;
+        sum+=a[i]-'0';
+        i++;
     }
-    return all_1;
-}
+    test=sum;
 
-int main ()
-{
-    int x,a,weishu_1,weishu_2,sum;
-    cin>>x;
-    weishu_1 = count_1(x);
-    sum= count_2(weishu_1,x);
-    weishu_2 = count_1(sum);
-    vector<int>obj;
-    for(int i = 0;i<weishu_2;i++)
+    while(1)
     {
-        a = sum%10;
+        n++;
+        test/=10;
+        if(test==0)  break;
+    }
+    test=sum;
+
+    while(test)
+    {
+        b*=10;
+        b+=test%10;
+        test/=10;
+    }
+    sum=b;
+
+    while(n)
+    {
+        if((sum%10<=9&&sum%10>=0)&&n!=1)
+            switch(sum%10)
+            {
+                case 0:printf("ling ");break;
+                case 1:printf("yi ");break;
+                case 2:printf("er ");break;
+                case 3:printf("san ");break;
+                case 4:printf("si ");break;
+                case 5:printf("wu ");break;
+                case 6:printf("liu ");break;
+                case 7:printf("qi ");break;
+                case 8:printf("ba ");break;
+                case 9:printf("jiu ");break;
+            }
+        else
+            switch(sum%10)
+            {
+                case 0:printf("ling");break;
+                case 1:printf("yi");break;
+                case 2:printf("er");break;
+                case 3:printf("san");break;
+                case 4:printf("si");break;
+                case 5:printf("wu");break;
+                case 6:printf("liu");break;
+                case 7:printf("qi");break;
+                case 8:printf("ba");break;
+                case 9:printf("jiu");break;
+            }
         sum/=10;
-        obj.push_back(a);
+        n--;
     }
-
-    for(int i = 0;i<weishu_2;i++)
-    {
-        cout<<p_number(obj[i])<<" ";
-    }
-
     return 0;
 }
-
-
-//
-// Created by Gypsophila on 2022/10/8.
-//
